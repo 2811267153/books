a314694c77313c80212dcea50decceb6c387c28c
徐泽森 <2811267153@qq.com>
2022-02-07 20:25:35 +0800

    适配HTTPS

--- src/api/home.js
+++ src/api/home.js
@@ -30,7 +30,7 @@ export function getOther(author) {
 // }
 //搜索关键字
 export function getBook(id) {
-	return axios.get(`http://novel.kele8.cn/book-info/${id}`)
+	return axios.get(`https://novel.kele8.cn/book-info/${id}`)
 }
 //获取 关于书籍章节详情
 export function bookChapters(id){
@@ -38,15 +38,15 @@ export function bookChapters(id){
 }
 //获取 书籍源
 export function getBookRoot(id){
-	return axios.get(`http://novel.kele8.cn/book-sources?view=summary&book=${id}`)
+	return axios.get(`https://novel.kele8.cn/book-sources?view=summary&book=${id}`)
 }
 //获取 相关书籍推荐
 export function getRecBook(id){
-	return axios.get(`http://novel.kele8.cn/recommend/${id}`)
+	return axios.get(`https://novel.kele8.cn/recommend/${id}`)
 }
 //获取 相关书籍推荐
 export function getBookContent(id, order){
-	return axios.get(`http://novel.kele8.cn/api/chapters2/${id}/${order}`)
+	return axios.get(`https://novel.kele8.cn/api/chapters2/${id}/${order}`)
 }
 //获取 相关书籍推荐
 // export function bookChapters(id){
@@ -55,7 +55,7 @@ export function getBookContent(id, order){
 
 //获取 频道小说
 export function getSex(){
-	return axios.get(`http://novel.kele8.cn/rank-category`)
+	return axios.get(`https://novel.kele8.cn/rank-category`)
 }
 //获取 排行榜详情
 export function getSexInfo(id){
